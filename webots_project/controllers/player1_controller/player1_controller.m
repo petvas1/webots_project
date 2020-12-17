a8010d9f33fbca26044db54c336d2a525dfdc616
PetoVas <74054205+PetoVas@users.noreply.github.com>
2020-12-17 12:00:06 +0100

    uploading upraged version

---
+++ webots_project/controllers/player1_controller/player1_controller.m
@@ -0,0 +1,36 @@
+TIME_STEP = 64;
+
+MAX_SPEED = 6.28;
+
+left_motor = wb_robot_get_device('left wheel motor');
+right_motor = wb_robot_get_device('right wheel motor');
+wb_motor_set_position(left_motor, inf);
+wb_motor_set_position(right_motor, inf);
+<<<<<<< Updated upstream
+=======
+wb_motor_set_velocity(left_motor, 0);
+wb_motor_set_velocity(right_motor, 0);
+
+% movement of the robot, initializing variables
+sides = 9;   % polygon size 
+side_length = 0.13;
+wheel_radius = 0.0075;
+linear_velocity = wheel_radius*max_speed;
+side_time = side_length/linear_velocity;
+time_start = wb_robot_get_time;
+>>>>>>> Stashed changes
+
+wb_motor_set_velocity(left_motor, MAX_SPEED);
+wb_motor_set_velocity(right_motor, MAX_SPEED);
+
+gps = wb_robot_get_device('gps');
+wb_gps_enable(gps, TIME_STEP);
+
+while wb_robot_step(TIME_STEP) ~= -1
+% wb_console_print('Hello!');
+% x_y_z_array = wb_gps_get_values(gps);
+values = wb_gps_get_values(gps);
+wb_console_print(sprintf('MY_ROBOT is at position: %g %g %g\n', values(1), values(2), values(3)), WB_STDOUT);
+speed = wb_gps_get_speed(gps);
+
+end
---
+++ webots_project/controllers/player3_controller/player3_controller.m
@@ -0,0 +1,58 @@
+% initialize basic info
+time_step = 64;
+max_speed = 6.28; %angular velocity
+
+% set robot position and velocity
+left_motor = wb_robot_get_device('left wheel motor');
+right_motor = wb_robot_get_device('right wheel motor');
+wb_motor_set_position(left_motor, inf);
+wb_motor_set_position(right_motor, inf);
+wb_motor_set_velocity(left_motor, 0);
+wb_motor_set_velocity(right_motor, 0);
+
+% movement of the robot, initializing variables
+sides = 6;   % polygon size 
+side_length = 0.12;
+wheel_radius = 0.0075;
+linear_velocity = wheel_radius*max_speed;
+side_time = side_length/linear_velocity;
+time_start = wb_robot_get_time;
+
+% turning
+rotation_angle = 6.28/sides;
+distance_btw_wheels = 0.052;
+rotation_rate = 2*linear_velocity/distance_btw_wheels;
+turn_duration = 0.5*rotation_angle/rotation_rate;
+rotation_start_time = time_start + side_time;
+rotation_end_time = rotation_start_time + turn_duration;
+
+% gps 
+gps = wb_robot_get_device('gps');
+wb_gps_enable(gps, time_step*70);
+
+% main loop:
+% -perform simulation steps
+while wb_robot_step(time_step) ~= -1
+% movement of the robot
+  left_speed = max_speed;
+  right_speed = max_speed;
+  % go backwards randomly
+     values = wb_gps_get_values(gps);
+    if values(1) > 0
+      left_speed = -max_speed;
+      right_speed = -max_speed;
+    end
+  % change direction after side_lenght is traveled 
+  current_time = wb_robot_get_time;
+    if (rotation_start_time < current_time) && (current_time < rotation_end_time) 
+      left_speed = max_speed;
+      right_speed = -max_speed;
+    elseif current_time > rotation_end_time
+      rotation_start_time = current_time + side_time;
+      rotation_end_time = rotation_start_time + turn_duration;
+    end
+    
+  wb_motor_set_velocity(left_motor, left_speed);
+  wb_motor_set_velocity(right_motor, right_speed);
+
+end
